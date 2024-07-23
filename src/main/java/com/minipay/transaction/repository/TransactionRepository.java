706c1d3c547ce3e0653d71ff554b0de64c82051a
jeongjiwon <jungkt0x@naver.com>
2024-07-23 14:59:46 +0900

    feat : 송금 내역 조회 기능 추가

--- src/main/java/com/minipay/transaction/repository/TransactionRepository.java
+++ src/main/java/com/minipay/transaction/repository/TransactionRepository.java
@@ -1,9 +1,17 @@
 package com.minipay.transaction.repository;
 
+import com.minipay.account.domain.Account;
 import com.minipay.transaction.domain.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    @Query("SELECT t FROM Transaction t WHERE t.receiverAccount = :account OR t.senderAccount = :account")
+    List<Transaction> findByReceiverOrSenderAccount(@Param("account") Account account);
 }
