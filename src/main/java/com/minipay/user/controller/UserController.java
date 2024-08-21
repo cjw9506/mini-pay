f4caf09e637b602ecb9741f34f1eb67a88f3323d
jeongjiwon <jungkt0x@naver.com>
2024-08-21 15:01:08 +0900

    docs: 회원가입 api 설명 추가

--- src/main/java/com/minipay/user/controller/UserController.java
+++ src/main/java/com/minipay/user/controller/UserController.java
@@ -2,6 +2,9 @@ package com.minipay.user.controller;
 
 import com.minipay.user.dto.SignupDTO;
 import com.minipay.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "User", description = "User API")
 @RestController
 @Getter
 @RequiredArgsConstructor
@@ -18,6 +22,8 @@ public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "회원가입", description = "유저의 회원가입을 진행합니다.")
+    @ApiResponse(responseCode = "200", description = "성공")
     @PostMapping("/signup")
     public ResponseEntity<?> signup(@RequestBody SignupDTO request) {
         userService.save(request);
